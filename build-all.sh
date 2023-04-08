b3a1adc35978cb616e8dfcbf127e6da341c99129
dimi309 <dimi309@users.noreply.github.com>
2023-04-08 17:46:59 +0200

    Corrected Linux / MacOS build.

--- build-all.sh
+++ build-all.sh
@@ -1,27 +1,18 @@
 set -e
+
 if [ "$1" != "Debug" ] && [ "$1" != "Release" ]; then
     echo "Please indicate build type: Debug or Release, followed by opengl if you would like to also prepare OpenGL-related libraries."
     exit 1
 fi
 
-if [ ! -z "$2" ] && [ "$2" != "opengl" ]; then
-    echo "The second parameter, if entered, can only be opengl if you would like to build for OpenGL."
-    exit 1
+if [ ! -d "small3d/build" ]; then
+   cd small3d
+   cd scripts
+   ./build.sh $1 $2
+   cd ../..
 fi
 
-git clone https://github.com/dimi309/small3d
-rm -rf small3d/.git
-cd small3d
-cd scripts
-./build.sh $1 $2
-cd ..
-cd ..
-
-if [ "$2" == "opengl" ]; then
-    export CMAKE_DEFINITIONS="-DCMAKE_BUILD_TYPE=$1 -DSMALL3D_OPENGL=ON"
-else
-    export CMAKE_DEFINITIONS=-DCMAKE_BUILD_TYPE=$1
-fi
+export CMAKE_DEFINITIONS=-DCMAKE_BUILD_TYPE=$1
 
 for sampledir in avoidthebug chasethegoat frogremixed serial-model-renderer
 do
